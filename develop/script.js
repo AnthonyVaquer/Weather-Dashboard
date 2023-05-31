3a05bd4ed354d3da337532dd724fb2fc28dca050
Anthony Vaquer <anthonymark126@gmail.com>
2023-05-30 19:31:52 -0700

    removed commented out code

--- develop/script.js
+++ develop/script.js
@@ -21,33 +21,35 @@ function getCurrentDate() {
   const currentDate = dayjs().format("dddd, MMMM D, YYYY");
   return currentDate;
 }
+
 // display current date
 currentDateEl.textContent = getCurrentDate();
 
 
 formEl.addEventListener('submit', handleFormSubmit);
-// historyListEl.addEventListener('click', handleHistoryItemClick);
 
-// Function to handle form submit
+// submit the search form
 async function handleFormSubmit(event) {
   event.preventDefault();
 
-  // Get the city name from the input
+  // get the city name from the input
   const cityName = cityInputEl.value.trim();
 
   if (cityName) {
-    // Clear the input
+    // clear the input
     cityInputEl.value = '';
 
-    // Get the current weather and forecast for the city
+    // get the current weather and forecast for the city
     const location = await getCurrentWeather(cityName);
     const forecast = await getForecast(location);
     displayForecast(forecast);
 
-    // Add the city to the search history
+    // add the city to the search history
     addCityToHistory(cityName);
   }
 }
+
+// getCurrentWeather function
 async function getCurrentWeather(cityName) {
 	//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
  const response = await fetch('https://api.openweathermap.org/geo/1.0/direct?q=' +
@@ -57,24 +59,9 @@ async function getCurrentWeather(cityName) {
         const jsonData = await response.json();
         console.log(jsonData);
         return jsonData[0];
-        // fetch(weatherApiRootUrl +
-        // `/data/2.5//forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`)
-        // .then(forecast => {
-        //   console.log(forecast);
-        // })
-    
-	// var jsonResponse1 = await response1.json();
-	// console.log(jsonResponse1);
-
-	// var lat = jsonResponse1[0].lat;
-	// var lon = jsonResponse1[0].lon;
-
-	// var response2 = await fetch(
-		
-	// );
-	// var jsonResponse2 = await response2.json();
-	// console.log(jsonResponse2);
 }
+
+// getForecast function
 async function getForecast (location) {
    const response = await fetch(weatherApiRootUrl +
         `/data/2.5//forecast?lat=${location.lat}&lon=${location.lon}&appid=${weatherApiKey}`)
@@ -84,23 +71,21 @@ async function getForecast (location) {
         return jsonData
   
 }
-// let forecastArea = document.createElement('div');
-// let 
 
 function displayForecast(forecast) {
-  // Update the city name
+  // get the city name
   cityNameEl.textContent = forecast.city.name;
-  // Get the current weather data
+  // get the current weather data
   const currentWeather = forecast.list[0].main;
-  // Update the temperature
+  // get the temperature
   const temperature = currentWeather.temp;
   temperatureEl.textContent = `Temperature: ${temperature}°C`;
-  // Update the humidity
+  // get the humidity
   const humidity = currentWeather.humidity;
   humidityEl.textContent = `Humidity: ${humidity}%`;
-  // Get the wind data
+  // get the wind data
   const wind = forecast.list[0].wind;
-  // Update the wind speed
+  // updated the wind speed
   const windSpeed = wind.speed;
   windSpeedEl.textContent = `Wind Speed: ${windSpeed} m/s`;
 }
@@ -119,9 +104,9 @@ function addCityToHistory(city) {
 
 function renderSearchHistory() {
   historyListEl.innerHTML = '';
-  // Retrieve the search history from local storage
+  // get search history from local storage
   const storedHistory = localStorage.getItem('searchHistory');
-  // Parse the stored history as an array or use an empty array if it doesn't exist
+  // prase the stored history as an array or use an empty array if it doesn't exist
   const parsedHistory = storedHistory ? JSON.parse(storedHistory) : [];
 
   history = parsedHistory;
@@ -135,32 +120,7 @@ function renderSearchHistory() {
   });
 }
 
-// Loads search history
+// loads search history
 history = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
 renderSearchHistory();
-
-
-
-
-
-
-
-{/* <div id="current-weather">
-      <h2 id="city-name"></h2>
-      <h3 id="current-date"></h3>
-      <img id="weather-icon" src="" alt="Weather Icon">
-      <p id="temperature"></p>
-      <p id="humidity"></p>
-      <p id="wind-speed"></p>
-    </div>
-    
-    <div id="forecast">
-      <h2>5-Day Forecast</h2>
-      <ul id="forecast-list"></ul>
-    </div>
-    
-    <div id="search-history">
-      <h3>Search History</h3>
-      <ul id="history-list"></ul>
-    </div> */}
